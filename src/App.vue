1d0acc55aebe331a5170e556185eeb5ec538f985
jakefish18 <jakefish.work@gmail.com>
2024-10-11 22:33:08 +0300

    Basic project setup

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  Hi
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Inter sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+body {
+  background-color: #232B2B;
+}
+</style>
